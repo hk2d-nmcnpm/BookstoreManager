e3ec832e2e005a9584092c4f07aee0106b055b17
Hiếu Trần Trung <17520484@ms.uit.edu.vn>
2019-05-22 21:13:45 +0700

    add DBConnection class

---
+++ DataAccessLayer/DBConnection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+
+namespace DataAccessLayer
+{
+    class DBConnection
+    {
+        const string connectionString = "Data Source=DESKTOP-9EOMK9V;Initial Catalog=bookstore-manager;Integrated Security=True";
+        protected OleDbConnection _connection;
+        public DBConnection()
+        {
+            try
+            {
+                _connection = new OleDbConnection(connectionString);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
